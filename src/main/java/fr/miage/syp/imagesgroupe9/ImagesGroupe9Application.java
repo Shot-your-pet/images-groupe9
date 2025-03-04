51199d8a9eb3a8f8a310bf328a5e21ba100a73d7
Maxime Galaxy Book <max.sevot@gmail.com>
2025-03-04 15:13:02 +0100

    Init commit

---
+++ src/main/java/fr/miage/syp/imagesgroupe9/ImagesGroupe9Application.java
@@ -0,0 +1,13 @@
+package fr.miage.syp.imagesgroupe9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesGroupe9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagesGroupe9Application.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/syp/imagesgroupe9/ImagesGroupe9ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.syp.imagesgroupe9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesGroupe9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
